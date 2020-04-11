8bf971cfa156a33741a3a86d5f84f0520c80a135
Andrzej Kwiecien <kwiecien_andrzej@interia.pl>
2020-04-11 10:59:15 +0200

    2059

--- lam_join.py
+++ lam_join.py
@@ -7,7 +7,7 @@ def join_por(kick_off_date):
     aggregeted_csv = []
     for file in files:
         if file.startswith("LAM_BRA"):
-            fr = open(os.path.join(dir_path, kick_off_date, file), 'r')
+            fr = open(os.path.join(dir_path, kick_off_date, file), 'r', encoding="utf-8")
             for line in fr:
                 if line.startswith("place_id"):
                     if len(aggregeted_csv)==0:
@@ -16,9 +16,9 @@ def join_por(kick_off_date):
                     aggregeted_csv.append(line)
             os.remove(file)
 
-    fw = open(os.path.join(dir_path, kick_off_date, "LAM_POR_"+kick_off_date+".csv"), 'w')
+    fw = open(os.path.join(dir_path, kick_off_date, "LAM_POR_"+kick_off_date+".csv"), 'w', encoding="utf-8")
     for item in aggregeted_csv:
-        fw.write(item.encode('utf8'))
+        fw.write(item)
 
 def join_spa(kick_off_date):
     dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -26,7 +26,7 @@ def join_spa(kick_off_date):
     aggregeted_csv = []
     for file in files:
         if not file.startswith("LAM_BRA"):
-            fr = open(os.path.join(dir_path, kick_off_date, file), 'r')
+            fr = open(os.path.join(dir_path, kick_off_date, file), 'r', encoding="utf-8")
             for line in fr:
                 if line.startswith("place_id"):
                     if len(aggregeted_csv)==0:
@@ -35,7 +35,7 @@ def join_spa(kick_off_date):
                     aggregeted_csv.append(line)
             os.remove(os.path.join(dir_path, kick_off_date, file))
 
-    fw = open(os.path.join(dir_path, kick_off_date, "LAM_SPA_"+kick_off_date+".csv"), 'w')
+    fw = open(os.path.join(dir_path, kick_off_date, "LAM_SPA_"+kick_off_date+".csv"), 'w', encoding="utf-8")
     for item in aggregeted_csv:
         fw.write(item.encode('utf8'))
 
