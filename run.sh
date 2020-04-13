12f5a79e7ca3c8a6f9b5824909bdd7408685cf85
Andrzej Kwiecien <kwiecien_andrzej@interia.pl>
2020-04-13 09:18:14 +0200

    asdas

--- run.sh
+++ run.sh
@@ -29,7 +29,7 @@ do
 done
 
 # EEU - change true or false if next week EEU is required or not -------------------------------------------------------------------------------------------------
-EEU_countries=("TUR" "POL" "RUS" "RUM" "GRC" "CHE" "HUN" "HRV" "BGR" "SVK" "KAZ")
+EEU_countries=("TUR" "POL" "RUS" "ROU" "GRC" "CHE" "HUN" "HRV" "BGR" "SVK" "KAZ")
 # EEU_countries=("SVK")
 if $EEU_to_do
 then
